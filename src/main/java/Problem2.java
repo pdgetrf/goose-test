4135d97807525c6703c5d76eb9e5bef2cc55fb27
Peng Du <rick.peng.du@gmail.com>
2021-01-31 21:22:58 -0800

    some changes

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,7 +2,6 @@ public class Problem2 {
 
     // do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
-        // add your solution
         return; // place holder
     }
 }
