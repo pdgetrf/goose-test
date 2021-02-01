f461cd6f09d065120747a0d5a4c75236ffac441e
Peng Du <rick.peng.du@gmail.com>
2021-01-31 22:33:49 -0800

    fix

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,19 +1,21 @@
 public class Problem1 {
 
- public static int binarySearch(int[] nums, int target) {
-     int start = 0;
-     int end = nums.length - 1;
-     while (start <= end){int mid =
-             (start + end) / 2;
-         if (nums[mid] == target) {
-             return mid
-                     ;
-         }
-         if (target > nums[mid]) { start = mid + 1;
-         } else {
-             end = mid - 1;
-         }
-     }
-     return -1;
- }
+    public static int binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid =
+                    (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid
+                        ;
+            }
+            if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
