76129be55380dd242e6bd42f0902b65cac516f56
Peng Du <rick.peng.du@gmail.com>
2021-01-31 22:59:47 -0800

    reformat

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,7 +1,7 @@
 public class Problem2 {
 
-    // do not change signature (function name, parameters)
-    public static void bubbleSort(int[] data) {
-        return; // place holder
-    }
+  // do not change signature (function name, parameters)
+  public static void bubbleSort(int[] data) {
+    return; // place holder
+  }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -4,13 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 public class Problem2Test {
 
-    // example test
-    // okay to change
-    @Test
-    public void testBubbleSort() {
-        assertTrue(false); // place holder
-        return;
-    }
+  // example test
+  // okay to change
+  @Test
+  public void testBubbleSort() {
+    assertTrue(false); // place holder
+    return;
+  }
 
-    /* add more tests here */
+  /* add more tests here */
 }
