797dd4576cfe6ac937c6fe9c30f3c93118ca9ac5
Peng Du <rick.peng.du@gmail.com>
2021-03-05 22:19:39 -0800

    add script

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-echo "Hello World"
-date
+
+input="readme.md"
+
+while IFS= read -r line
+do
+  if [[ "$line" == *"due date"* ]]; then
+    duedate=`echo $line | sed 's/due date: \(.*\)/\1/'`
+    echo $duedate
+  fi
+done < "$input"
+
+date=`date`
+
+todateepoch=$(date -d "$date" +%s)
+
+duedateepoch=$(date -d "$duedate" +%s)
+
+echo "$duedate - $todate" 
+
+if [ "$todateepoch" > "$duedateepoch" ] ;
+then
+    echo "your late"
+fi
+
