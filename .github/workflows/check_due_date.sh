8240f3e3bd7fc5df3e8f6e922a6473c6958ba2c4
Peng Du <rick.peng.du@gmail.com>
2021-03-08 13:16:22 -0800

    add script

---
+++ .github/workflows/check_due_date.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git branch
+
+git remote -v
+
+input="readme.md"
+
+while IFS= read -r line
+do
+  if [[ "$line" == *"due date"* ]]; then
+    duedate=`echo $line | sed 's/due date: \(.*\)/\1/'`
+    echo $duedate
+  fi
+done < "$input"
+
+todate=`TZ=PST8PDT date`
+
+echo "today: $todate"
+
+echo "due date: $duedate"
+
+todateepoch=$(date -d "$todate" +%s)
+duedateepoch=$(date -d "$duedate" +%s)
+
+if [ "$todateepoch" -gt "$duedateepoch" ] ;
+then
+    echo "past due!"
+    exit 1
+else
+    echo "all good"
+fi
+
